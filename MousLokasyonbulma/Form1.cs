65ac9ff234bd792dd10546732f25c0550a7e4110
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-06-05 07:21:52 +0300

    Mous Lokasyon Bulma Programı
    Mous Lokasyon Bulma Programı

---
+++ MousLokasyonbulma/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MousLokasyonbulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int x = 0, y = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = (x = Cursor.Position.X).ToString();
+            textBox2.Text = (y = Cursor.Position.Y).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = (x = Cursor.Position.X).ToString();
+            textBox2.Text = (y = Cursor.Position.Y).ToString();
+            timer1.Start();
+        }
+    }
+}
